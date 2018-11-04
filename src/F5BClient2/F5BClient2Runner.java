3cdb791a47f447791ed7e6707fda556066d60154
be007206 <be007206@dieters-laptop>
2018-11-04 16:25:08 +0100

    Constant naming conventions followed

--- src/F5BClient2/F5BClient2Runner.java
+++ src/F5BClient2/F5BClient2Runner.java
@@ -2,10 +2,10 @@ package F5BClient2;
 
 public class F5BClient2Runner {
     
-    private static final String version = "v2.0"; 
+    private static final String VERSION = "v2.0"; 
     
     public static void main(String[] args) throws Exception {
         F5BClient2 mainApp = new F5BClient2();
-        mainApp.start(version);
+        mainApp.start(VERSION);
     }
 }
