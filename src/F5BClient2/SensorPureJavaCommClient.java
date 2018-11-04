200fe59e01ffa3edb0cc542c8b644650d0d70a52
be007206 <be007206@dieters-laptop>
2018-11-04 15:08:39 +0100

    More looping fixes and removed debug messages

--- src/F5BClient2/SensorPureJavaCommClient.java
+++ src/F5BClient2/SensorPureJavaCommClient.java
@@ -28,6 +28,7 @@ public class SensorPureJavaCommClient {
     public boolean serialConnectOk = false;
     private final int telemetryNotOkMaxCounter = 20;
     private final long screenUpdateInterval = 3000;
+    private final String port = "/dev/ttyUSB0";
     byte[] message;
     String originalValue;
     
@@ -56,10 +57,9 @@ public class SensorPureJavaCommClient {
                         while (!serialConnectOk) {
                             lcd_gpio_Handler.writeLineWithDate("Connecting Radio");
                             try {
-                                portId = CommPortIdentifier.getPortIdentifier("/dev/ttyUSB0");
+                                portId = CommPortIdentifier.getPortIdentifier(port);
                             } catch (NoSuchPortException ex) {
-                                Logger.getLogger(SensorPureJavaCommClient.class.getName()).log(Level.SEVERE, null, ex);
-                                System.out.println("Unable to open port: " + ex.toString());
+                                System.out.println("Unable to open port: " + port + " => " + ex.toString());
                                 Thread.sleep(1000);
                             }
                             if (portId != null) {
@@ -72,83 +72,87 @@ public class SensorPureJavaCommClient {
                                 inputStr = serialPort.getInputStream();
                                 serialPort.notifyOnDataAvailable(true);
                                 serialConnectOk = true;
-                            }
+                                System.out.println("USB Port Configured!");
+                            
+                                serialPort.addEventListener(new SerialPortEventListener() {
+                                    @Override                                
+                                    public void serialEvent(SerialPortEvent event) {
+                                        //System.out.println("Portevent detected: " + event.getEventType());
+                                        try {
+                                            switch (event.getEventType() ) {
+                                                case SerialPortEvent.DATA_AVAILABLE: 
+                                                    //System.out.println("Data Available");
+                                                    String data = "";
+                                                    length = inputStr.available();
+                                                    if (length > 0) {
+                                                        sensorReadTime = System.currentTimeMillis();
+                                                        message = new byte[length];
+                                                        inputStr.read(message);
+                                                        //System.out.println(System.currentTimeMillis() + "Data length: " + length + " / Data: " + message.toString());
 
-                            serialPort.addEventListener(new SerialPortEventListener() {
-                                @Override                                
-                                public void serialEvent(SerialPortEvent event) {
-                                    //System.out.println("Portevent detected: " + event.getEventType());
-                                    try {
-                                        switch (event.getEventType() ) {
-                                            case SerialPortEvent.DATA_AVAILABLE: 
-                                                System.out.println("Data Available");
-                                                String data = "";
-                                                length = inputStr.available();
-                                                if (length > 0) {
-                                                    sensorReadTime = System.currentTimeMillis();
-                                                    message = new byte[length];
-                                                    inputStr.read(message);
-                                                    System.out.println(System.currentTimeMillis() + "Data length: " + length + " / Data: " + message.toString());
-
-                                                    ByteBuffer unescapedMessage = unesc(message);
-                                                    unescapedMessage.flip();
-                                                    unescapedMessage.rewind();
-                                                    byte[] sens1 = new byte[3];
-                                                    byte[] sens2 = new byte[3];
-                                                    SensorReading sensValue1 = null;
-                                                    SensorReading sensValue2 = null;
-                                                    if (length > 19 && message[0] == 0x02 && message[length - 1] == 0x03 && checks(unescapedMessage)) {
-                                                        //System.out.println("Message OK");
+                                                        ByteBuffer unescapedMessage = unesc(message);
+                                                        unescapedMessage.flip();
                                                         unescapedMessage.rewind();
-                                                        if (message[7] == 6) {
-                                                            //System.out.println("7th position of original message = 6, so telemetry is being received by transmitter");
-                                                            unescapedMessage.position(8);
-                                                            //Long when = timBytes.getWhen();
-                                                            unescapedMessage.get(sens1, 0, 3);
-                                                            sensValue1 = treatSensor(sens1);
-                                                            unescapedMessage.get(sens2, 0, 3);
-                                                            sensValue2 = treatSensor(sens2);
+                                                        byte[] sens1 = new byte[3];
+                                                        byte[] sens2 = new byte[3];
+                                                        SensorReading sensValue1 = null;
+                                                        SensorReading sensValue2 = null;
+                                                        if (length > 19 && message[0] == 0x02 && message[length - 1] == 0x03 && checks(unescapedMessage)) {
+                                                            //System.out.println("Message OK");
+                                                            unescapedMessage.rewind();
+                                                            if (message[7] == 6) {
+                                                                //System.out.println("7th position of original message = 6, so telemetry is being received by transmitter");
+                                                                unescapedMessage.position(8);
+                                                                //Long when = timBytes.getWhen();
+                                                                unescapedMessage.get(sens1, 0, 3);
+                                                                sensValue1 = treatSensor(sens1);
+                                                                unescapedMessage.get(sens2, 0, 3);
+                                                                sensValue2 = treatSensor(sens2);
+                                                            } else {
+                                                                //System.out.println("No telemetry from receiver!");
+                                                                telemetryNotOkSetter();
+                                                            }
                                                         } else {
-                                                            //System.out.println("No telemetry from receiver!");
+                                                            //System.out.println("Message NOT OK");
                                                             telemetryNotOkSetter();
-                                                        }
-                                                    } else {
-                                                        //System.out.println("Message NOT OK");
-                                                        telemetryNotOkSetter();
-                                                    }    
-                                                }
-                                                break;
-                                            default:
-                                                System.out.println("No data available");
-                                                break;
+                                                        }    
+                                                    }
+                                                    break;
+                                                default:
+                                                    System.out.println("No data available");
+                                                    break;
+                                            }
+                                        } 
+                                        catch (Exception e) {
+                                            System.out.println("Error reading data: " + e.toString());
+                                            serialConnectOk = false;
+                                            if (serialPort != null) {
+                                                serialPort.close();
+                                            }
+                                            portId = null;
                                         }
-                                    } 
-                                    catch (Exception e) {
-                                        System.out.println("Error reading data: " + e.toString());
-                                        serialConnectOk = false;
-                                        //e.printStackTrace();
-                                    }
-                                }                    
-                            });
-                        
-                            //System.out.println("TimeDiff: " + Long.toString(System.currentTimeMillis() - sensorReadTime));
-                            if (System.currentTimeMillis() - sensorReadTime < 2000) {
-                                //System.out.println("Updating screen with WMin/Alt");
-                                Lcd.lcdClear(lcd_gpio_Handler.lcdHandle);
-                                Lcd.lcdPosition (lcd_gpio_Handler.lcdHandle, 0, 0);
-                                Lcd.lcdPuts (lcd_gpio_Handler.lcdHandle, Handler.formatTextToFit1Line("WMin:" + wMin));
-                                Lcd.lcdPosition (lcd_gpio_Handler.lcdHandle, 0, 1);
-                                Lcd.lcdPuts (lcd_gpio_Handler.lcdHandle, Handler.formatTextToFit1Line("Alt:" + altitude));
-                                Thread.sleep(screenUpdateInterval);
-                            } else {
-                                Lcd.lcdClear(lcd_gpio_Handler.lcdHandle);
-                                Lcd.lcdPosition (lcd_gpio_Handler.lcdHandle, 0, 0);
-                                Lcd.lcdPuts (lcd_gpio_Handler.lcdHandle, Handler.formatTextToFit1Line("Last WMin:" + wMin));
-                                Lcd.lcdPosition (lcd_gpio_Handler.lcdHandle, 0, 1);
-                                Lcd.lcdPuts (lcd_gpio_Handler.lcdHandle, Handler.formatTextToFit1Line("Sensor Dead"));
-                                Thread.sleep(screenUpdateInterval);
+                                    }                    
+                                });
                             }
                         }
+                        //System.out.println("TimeDiff: " + Long.toString(System.currentTimeMillis() - sensorReadTime));
+                        if (System.currentTimeMillis() - sensorReadTime < 2000) {
+                            //System.out.println("Updating screen with WMin/Alt");
+                            Lcd.lcdClear(lcd_gpio_Handler.lcdHandle);
+                            Lcd.lcdPosition (lcd_gpio_Handler.lcdHandle, 0, 0);
+                            Lcd.lcdPuts (lcd_gpio_Handler.lcdHandle, Handler.formatTextToFit1Line("WMin:" + wMin));
+                            Lcd.lcdPosition (lcd_gpio_Handler.lcdHandle, 0, 1);
+                            Lcd.lcdPuts (lcd_gpio_Handler.lcdHandle, Handler.formatTextToFit1Line("Alt:" + altitude));
+                            Thread.sleep(screenUpdateInterval);
+                        } else {
+                            Lcd.lcdClear(lcd_gpio_Handler.lcdHandle);
+                            Lcd.lcdPosition (lcd_gpio_Handler.lcdHandle, 0, 0);
+                            Lcd.lcdPuts (lcd_gpio_Handler.lcdHandle, Handler.formatTextToFit1Line("Last WMin:" + wMin));
+                            Lcd.lcdPosition (lcd_gpio_Handler.lcdHandle, 0, 1);
+                            Lcd.lcdPuts (lcd_gpio_Handler.lcdHandle, Handler.formatTextToFit1Line("Sensor Dead"));
+                            Thread.sleep(screenUpdateInterval);
+                        }
+                        
                     } catch (InterruptedException ex) {
                         Logger.getLogger(SensorPureJavaCommClient.class.getName()).log(Level.SEVERE, null, ex);
                     } catch (PortInUseException ex) {
@@ -161,7 +165,11 @@ public class SensorPureJavaCommClient {
                         System.out.println("Unsupported communication operation");
                         serialConnectOk = false;
                     } catch (IOException ex) {
-                        System.out.println("IOException");
+                        System.out.println("USB Port Disconnected");
+                        if (serialPort != null) {
+                            serialPort.close();
+                        }
+                        portId = null;
                         serialConnectOk = false;
                     }
                 }
